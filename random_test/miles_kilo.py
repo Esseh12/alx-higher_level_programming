9b28b5693ac53b190fe3d104551030da9ba5e684
Esseh12 <gbenmoese788@gmai.com>
2024-01-05 04:17:07 -0800

    a program that changes miles to kilometer

---
+++ random_test/miles_kilo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Problem: write a program that coverts miles to kilometer
+# Ask user to input a number in miles and save the input into a variable called miles
+miles = input("Enter a distance in miles: ")
+# Change the information saved into a float
+miles = float(miles)
+# convert the number in miless to kilometer and stote the value in a variable called kilo
+kilo = miles * 1.60934
+# print the value in kilo to the user
+print(f"{miles} miles = {kilo} kilometers")
