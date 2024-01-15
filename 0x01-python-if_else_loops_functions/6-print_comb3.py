739fcfe058d569737cfb1dafef9ba393ef1cfec5
Esseh12 <gbenmoese788@gmai.com>
2024-01-15 04:43:46 -0800

    merging master with testbranch

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
-
 #!/usr/bin/python3
-for n in range(9):
-    for j in range(n + 1, 10):
-        if n * 10 + n < 89:
-            print("{:d}{:d}".format(n, j), end=", ")
-print("{:d}".format(89))
+for i in range(10):
+    for j in range(10):
+        print("{:02d}, {:02d}".format(i, j), end=", " if j < 9 else "{:02d}, {:02d}\n".format(i, j))
