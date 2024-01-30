4bf59925c3f197f9fdb40db266662b1297bdc3ae
Esseh12 <gbenmoese788@gmai.com>
2024-01-30 14:39:16 -0800

    readme file

--- 0x03-python-data_structures/main_2.py
+++ 0x03-python-data_structures/main_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_list_integer = __import__('0-print_list_integer').print_list_integer
 
-my_list = [2, 6, 7, 5, 9]
-print_list_integer(my_list)
+list = []
+print_list_integer(list)
