74dbddcbea36ac2ff902d271898d9e74c6e93c0e
Esseh12 <gbenmoese788@gmai.com>
2024-02-08 04:04:44 -0800

    creating an empty class

---
+++ 0x06-python-classes/test_dir/README.md
@@ -0,0 +1,3 @@
+This is a text directory that contains all the test code i wrote for the project.
+this directory contains pratice code and doesn't run against the checker
+
---
+++ 0x06-python-classes/test_dir/empty_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is an empty class called shape
+"""
+class Shape:
+    pass
+
+#this is an object of thr class
+my_object = Shape()
+print(my_object)
