db95c8dc3b3882667c06528b46c1ef9f2c555971
Esseh12 <gbenmoese788@gmai.com>
2024-02-12 05:54:30 -0800

    if the size is greater than 0, use a loop to print #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""
+A class representing a shape square
+
+This class is currently empty
+"""
 
 
 class Square:
-    """Represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
-
+    """
+    how to create a class
+    """
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
-
+        return self.__size
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
+    def __init__(self, size=0):
+        self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        return self.__size ** 2
 
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
-            print("")
+            print()
+        else:
+            for _ in range(self.size):
+                print('#' *self.size)
