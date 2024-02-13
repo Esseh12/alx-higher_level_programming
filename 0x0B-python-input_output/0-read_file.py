c579dcadf982736df2814ceacc634f5a36ecc326
Esseh12 <gbenmoese788@gmai.com>
2024-02-13 09:12:37 -0800

    check

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
+
 """ 
     This module reads from a file
 """
 
+
 def read_file(filename=""):
 
+
 """ 
-    This funtion reads from a file
+This funtion reads from a file
+
+Args:
+        filename (str): The name of the file to read. If not provided, it reads "my_file_0.txt".
+
+
+Returns:
+None
 """
 
     with open("my_file_0.txt", encoding="UTF8") as f:
