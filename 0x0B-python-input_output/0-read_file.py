c587b22ea52aa8d2e8031a5751ed6f84dbbe8424
Esseh12 <gbenmoese788@gmai.com>
2024-02-13 05:56:23 -0800

    readme

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+
+
 def read_file(filename=""):
+    """Reads the contents of a text file and prints them to the standard output.
+
+    Args:
+        filename (str): The name of the file to read. If not provided, it reads "my_file_0.txt".
+
+    Returns:
+        None
+    """
+
     with open("my_file_0.txt", encoding="UTF8") as f:
         for line in f:
             print(line, end="")
