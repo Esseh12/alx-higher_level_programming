0035d8872725a358afc08fb078fd43bc65f0cf6b
Esseh12 <gbenmoese788@gmai.com>
2024-03-11 06:10:54 -0700

    raised an exception in the class BaseGeopmetry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    a class BaseGeometry
+    """
+
+    def area(self):
+        """
+        a public instance method that raises an Exception with
+        the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
