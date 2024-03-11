8e8bbb97f142b17395ac3b7cb73533449a9f0f28
Esseh12 <gbenmoese788@gmai.com>
2024-03-11 03:40:05 -0700

    added a type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,18 +2,19 @@
 
 
 """
-This module comtains a class that that returns True 
+This module comtains a class that that returns True
 if the object is exactly an instance of the specified class ; otherwise False.
 """
 
+
 def is_same_class(obj, a_class):
+
     """
-     a function that returns True if the object is exactly an 
+     a function that returns True if the object is exactly an
      instance of the specified class ; otherwise False
     """
 
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) == a_class:
         return True
     else:
         return False
-
