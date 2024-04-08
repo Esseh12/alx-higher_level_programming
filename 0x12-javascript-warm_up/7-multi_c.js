2ce57b6d0896d75bf9c6182cbe808b4d6c6a0232
Esseh12 <Gbenmoese788@gmailcom>
2024-04-08 22:57:18 +0100

    fixed parsing issue

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,10 +7,10 @@ const integer = parseInt(process.argv[2]);
 
 if (process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
-} else if (process.argv[2] > 0) {
+} else if (integer > 0) {
   const stringToRepeat = 'C is fun\n';
   const result = stringToRepeat.repeat(integer);
   console.log(result);
-} else if (process.argv[2] < 0) {
-  return;
+} else if (integer < 0) {
+  process.exit();
 }
