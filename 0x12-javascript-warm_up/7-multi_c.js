3a15785b6632467ee78a6bc5d8b8c27b138483a7
Esseh12 <Gbenmoese788@gmailcom>
2024-04-08 23:06:45 +0100

    fixed parsing issue

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -8,8 +8,18 @@ const integer = parseInt(process.argv[2]);
 if (isNaN(integer)) {
   console.log('Missing number of occurrences');
 } else if (integer > 0) {
-  const stringToRepeat = 'C is fun\n';
-  const result = stringToRepeat.repeat(integer);
+  //const stringToRepeat = 'C is fun\n';
+  //const result = stringToRepeat.repeat(integer);
+  //console.log(result);
+	
+  let result = '';
+  for (let i = 0; i < integer; i++) {
+    if (i < integer - 1) {
+      result += 'C is fun\n';
+    } else {
+      result += 'C is fun'; // Don't add a new line for the last repetition
+    }
+  }
   console.log(result);
 } else if (integer < 0) {
   process.exit();
