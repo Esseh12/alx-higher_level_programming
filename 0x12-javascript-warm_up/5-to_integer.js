39ae85a133a7b5841b77baaa5f30d3a45f4770f2
Esseh12 <Gbenmoese788@gmailcom>
2024-04-08 15:24:45 +0100

    a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+const processLenght = process.argv.length;
+
+const string = process.argv[2];
+const integer = parseInt(string);
+if (process.argv[2] === undefined){
+  console.log('Not a number');
+} else if (isNaN(process.argv[2]) !== true){
+  console.log('My number:' + ' ' + integer);
+} else {
+  console.log('Not a number')
+}
