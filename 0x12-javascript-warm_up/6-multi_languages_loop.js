b2b21234c799c8b55614b19001d933dcd6a01e45
Esseh12 <Gbenmoese788@gmailcom>
2024-04-08 22:04:51 +0100

    a script that prints 3 lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,8 @@
 
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
+// made some changes to fix semistandard issue
+
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
 }
