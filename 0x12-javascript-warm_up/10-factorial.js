faeeac07cf394521f24a0ffdee9f45cce7364a72
Esseh12 <Gbenmoese788@gmailcom>
2024-04-10 13:41:41 +0100

    solved a factorial question myself

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const process = require('process');
+const integer = parseInt(process.argv[2]);
+
+if (isNaN(integer)) {
+  console.log(1);
+} else if (isNaN(integer) === false){
+   function factorial(n) {
+   if (n === 0 || n === 1){
+      return 1;
+   } else {
+     return n * factorial(n - 1);
+  }
+ }
+  const Result = factorial(integer);
+  console.log(Result)
+}
