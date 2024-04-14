b7052566ce4b6e927d43f98d49b8c14f1f1d9fdc
Esseh12 <gbenmoese788@gmai.com>
2024-04-14 05:56:10 -0700

    fixing requirement error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,9 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
-    cur = conn.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     rows = cur.fetchall()
@@ -16,4 +16,4 @@ if __name__ == "__main__":
         print(row)
 
     cur.close()
-    conn.close()
+    db.close()
