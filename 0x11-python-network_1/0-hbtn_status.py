4364ae9e547df9d9288470b8a18efed3e4f41781
Esseh12 <Gbenmoese788@gmailcom>
2024-05-04 00:03:16 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
-""" create a Request object that specifies the URL"""
-
+"""a Python script that fetches 
+- https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(html)}")
-    print(f"\t- content: {html}")
-    print(f"\t- utf8 content: {html.decode('utf-8')}")
+        print('Body response:')
+        print(f"\t- type: {(type(html))}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
+
