afb5893041649e7ca23b5c913b8129b50e0b7ca8
Esseh12 <Gbenmoese788@gmailcom>
2024-05-03 14:37:50 +0100

    sending a post request using Curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # using curl to send a POST request
-curl -s -d email=test@gmail.com -d subject=I will always be here for PLD "$1"
+curl -s -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD' "$1"
