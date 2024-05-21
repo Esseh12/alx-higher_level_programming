a190f87900bd227cc4ee2189a6d99d7383860c0d
Esseh12 <Gbenmoese788@gmailcom>
2024-05-21 16:35:42 +0100

    fixed semistandard issue

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response) => {
-if (error){
-	console.error(error);
-	return;
-}
-console.log(`code: ${response.statusCode}`);
-})
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
+});
